3787fb86eb44b7760b11e4597cd56a897700b4da
sreekumar17 <absree17@gmail.com>
2025-01-28 18:13:57 +0530

    functionality changes has been done

---
+++ forms/newsletter.php
@@ -0,0 +1,21 @@
+<?php
+
+//Change this portion accrding to requirement
+
+  $receiving_email_address = 'contact@solutiontechservices.com';
+
+  $subject = 'Someone has subscribed newsletter';
+  $message = 'Someone has subscribed newsletter - '.
+              '\n Email - '.$_POST['email'];
+
+//-- Change this portion accrding to requirement
+
+  $from = $_POST['email'];
+  $headers = "" .
+           "Reply-To:" . $from . "\r\n" .
+           "From:" . $from . "\r\n" .
+           "X-Mailer: PHP/" . phpversion();
+  $headers .= 'MIME-Version: 1.0' . "\r\n";
+  $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n"; 
+  echo mail($receiving_email_address, $subject, $message, $headers);
+?>
